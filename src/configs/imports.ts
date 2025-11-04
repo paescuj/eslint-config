2b1d2505309f619741f2f3af17d5c60580a14bd3
Pascal Jufer <pascal-jufer@bluewin.ch>
2025-11-04 10:35:00 +0100

    Switch to import-lite

--- src/configs/imports.ts
+++ src/configs/imports.ts
@@ -1,5 +1,5 @@
 import type { FlatConfigItem } from '../types.js';
-import pluginImportX from 'eslint-plugin-import-x';
+import pluginImportLite from 'eslint-plugin-import-lite';
 import { GLOB_VUE, GLOBS_CODE } from '../globs.js';
 
 export function imports(): FlatConfigItem {
@@ -7,23 +7,22 @@ export function imports(): FlatConfigItem {
 		name: 'paescuj/imports',
 		files: GLOBS_CODE,
 		plugins: {
-			'import-x': pluginImportX,
+			'import-lite': pluginImportLite,
 		},
 		rules: {
 			// Internal checks, without resolving (left to TypeScript or tests)
-			'import-x/first': 'error',
-			'import-x/consistent-type-specifier-style': ['error', 'prefer-top-level'],
-			'import-x/no-duplicates': 'error',
-			'import-x/no-mutable-exports': 'error',
-			'import-x/no-named-as-default': 'error',
-			'import-x/no-self-import': 'error',
+			'import-lite/consistent-type-specifier-style': ['error', 'prefer-top-level'],
+			'import-lite/first': 'error',
+			'import-lite/no-duplicates': 'error',
+			'import-lite/no-mutable-exports': 'error',
+			'import-lite/no-named-default': 'error',
 		},
 	},	{
 		name: 'paescuj/imports/vue',
 		files: [GLOB_VUE],
 		rules: {
 			// Not enforced until there's a fix available, see https://github.com/vuejs/eslint-plugin-vue/issues/1577
-			'import-x/first': 'off',
+			'import-lite/first': 'off',
 		},
 	}];
 }
